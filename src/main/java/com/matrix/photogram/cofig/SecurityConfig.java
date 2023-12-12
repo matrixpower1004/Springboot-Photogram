b0fd33bc0f8c5f4327f3982599423cc77c2e8a28
matrixpower1004 <matrixpower1004@gmail.com>
2023-12-13 07:24:20 +0900

    Refactor: 스프링 시큐리티 최신버전 설정 반영
    - 최선 버전의 spec에 맞추어 @Bean 등록 방식으로 설정 변경

--- src/main/java/com/matrix/photogram/cofig/SecurityConfig.java
+++ src/main/java/com/matrix/photogram/cofig/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.matrix.photogram.cofig;
 
+import com.matrix.photogram.cofig.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import com.matrix.photogram.cofig.oauth.OAuth2DetailsService;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
-@EnableWebSecurity //해당 파일로 시큐리티를 활성화 
 @Configuration //IoC
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
 
 	private final OAuth2DetailsService oAuth2DetailsService;
 	
@@ -22,22 +18,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public BCryptPasswordEncoder encode() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		//super 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화
+
+	@Bean
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/*.*").authenticated() //인증이 필요한 요청
-			.anyRequest().permitAll() //그게 아닌 모든 요청은 허용
-			.and()
-			.formLogin()
-			.loginPage("/auth/signin") // GET
-			.loginProcessingUrl("/auth/signin")// POST -> 스프링 시큐리티가 로그인 프로세스 진행
-			.defaultSuccessUrl("/")
-			.and()
-			.oauth2Login() //form 로그인도 하는데, oauth2 로그인도 할꺼야. 
-			.userInfoEndpoint() //oauth2 로그인을 하면 최종 응답을 회원정보로 바로 받을 수 있다. 
-			.userService(oAuth2DetailsService);
+				.antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**").authenticated()
+				.anyRequest().permitAll()
+				.and()
+				.formLogin()
+				.loginPage("/auth/signin") // GET
+				.loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
+				.defaultSuccessUrl("/")
+				.and()
+				.oauth2Login() // form로그인도 하는데, oauth2로그인도 할꺼야!!
+				.userInfoEndpoint() // oauth2로그인을 하면 최종응답을 회원정보를 바로 받을 수 있다.
+				.userService(oAuth2DetailsService);
+		return http.build();
 	}
 }
