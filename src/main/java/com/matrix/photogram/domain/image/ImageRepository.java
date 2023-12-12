981c3ec9a08cf4fc3b715daf6d019dc4a277987f
matrixpower1004 <matrixpower1004@gmail.com>
2023-12-13 07:43:35 +0900

    Fix: 네이티브 쿼리 문제 수정
    - 네이티브 쿼리의 parameter 전달 방식을 @param 어노테이션으로 수정.

--- src/main/java/com/matrix/photogram/domain/image/ImageRepository.java
+++ src/main/java/com/matrix/photogram/domain/image/ImageRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ImageRepository extends JpaRepository<Image, Integer>{
 	// fromUsrId는 로그인한 사용자 id
 	@Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC", nativeQuery=true)
-	Page<Image> mStory(int principalId, Pageable pageable);
+	Page<Image> mStory(@Param("principalId") int principalId, Pageable pageable);
 
 	@Query(value = "SELECT i.* FROM image i INNER JOIN(SELECT imageId, count(imageId) as likeCount FROM likes GROUP BY imageId) c ON i.id = c.imageId ORDER BY likeCount DESC", nativeQuery = true)
 	List<Image> mPopular();
